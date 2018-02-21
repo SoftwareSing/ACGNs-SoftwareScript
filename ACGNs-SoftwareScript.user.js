b9b7dae9346943cef536b8ec0b383b17384a8dd9
SoftwareSing <SoftwareSing@gmail.com>
2018-02-22 05:00:16 +0800

    新增 LoginUser
    新增一個class LoginUser
    用於紀錄當前登入的使用者的資訊

--- ACGNs-SoftwareScript.user.js
+++ ACGNs-SoftwareScript.user.js
@@ -84,14 +84,7 @@ const { dbOrders } = require('./db/dbOrders.js');
 /*************************************/
 /************GlobalVariable***********/
 
-//會跨2區以上使用的全域變數放在這裡
-//如從stockSummary跨到accountInfo用的變數
-
 let serverType = 'normal';
-
-let myId = null; //當前登入的使用者ID
-let myHoldStock = []; //當前登入的使用者持有的股票, 在股市總覽可以直接抓到全部
-let myOrders = []; //當前登入的使用者未完成交易的買賣單, 在股市總覽可以直接抓到全部
 
 let othersScript = [];
 
@@ -608,6 +601,102 @@ class User {
     console.log(`---end computeTax(): ${tax}`);
 
     return tax;
+  }
+}
+
+class LoginUser extends User {
+  constructor() {
+    const id = Meteor.userId();
+    console.log(`create LoginUser: ${id}`);
+    super(id);
+    this.orders = [];
+    this.scriptVip = null; //vip的class還沒寫
+
+    this.directorsCache = [];
+
+    console.log('');
+  }
+
+  //可能是原本沒登入後來登入了，所以要寫入id，或是分身......
+  changeLoginUser() {
+    const id = Meteor.userId();
+    console.log(`LoginUser: new ID: ${id}`);
+    this.userId = id;
+  }
+
+  updateFullHoldStocks() {
+    console.log(`---start updateFullHoldStocks()`);
+
+    this.loadFromSessionstorage();
+
+    const serverDirectors = dbDirectors.find({ userId: this.userId }).fetch();
+    //避免多次不必要的重複寫入，檢查是否與快取的一模一樣
+    if (JSON.stringify(serverDirectors) !== JSON.stringify(this.directorsCache)) {
+      const oldHoldStocks = this.holdStocks;
+      this.holdStocks = [];
+      for (const c of serverDirectors) {
+        const oldC = oldHoldStocks.find((x) => {
+          return (x.companyId === c.companyId);
+        });
+        //從舊資料中獲取vip等級資訊，避免將vip資訊洗掉
+        const vipLevel = (oldC !== undefined) ? oldC.vip : null;
+        this.holdStocks.push({companyId: c.companyId, stocks: c.stocks, vip: vipLevel});
+      }
+
+      this.saveToSessionstorage();
+      this.directorsCache = serverDirectors;
+    }
+
+    console.log(`---end updateFullHoldStocks()`);
+  }
+
+  updateOrders() {
+    this.orders = dbOrders.find({ userId: this.userId }).fetch();
+  }
+
+
+  computeBuyOrdersMoney() {
+    console.log(`---start computeBuyOrderMoney()`);
+
+    let money = 0;
+    for (const order of this.orders) {
+      if (order.orderType === '購入') {
+        money += order.unitPrice * (order.amount - order.done);
+      }
+    }
+
+    console.log(`---end computeBuyOrderMoney(): ${money}`);
+
+    return money;
+  }
+
+  computeSellOrdersAsset() {
+    console.log(`---start computeSellOrdersAsset()`);
+
+    let asset = 0;
+    const localCompanies = JSON.parse(window.sessionStorage.getItem('localCompanies')) || [];
+    for (const order of this.orders) {
+      if (order.orderType === '賣出') {
+        const companyData = localCompanies.find((x) => {
+          return (x.companyId === order.companyId);
+        });
+        //以參考價計算賣單股票價值, 如果找不到資料則用賣單價格
+        const price = (companyData !== undefined) ? companyData.price : order.unitPrice;
+        asset += price * (order.amount - order.done);
+      }
+    }
+
+    console.log(`---end computeSellOrdersAsset(): ${asset}`);
+
+    return asset;
+  }
+
+  //Override
+  computeTotalWealth() {
+    const totalWealth = super.computeTotalWealth() +
+      this.computeBuyOrderMoney() + this.computeSellOrdersAsset();
+
+    return totalWealth;
   }
 }
 
