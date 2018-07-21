25ff751eaa81da8b92558801bea822f4b3e007f2
SoftwareSing <SoftwareSing@gmail.com>
2018-07-21 18:42:52 +0800

    重構 MergeFile.py

--- MergeFile.py
+++ MergeFile.py
@@ -27,36 +27,50 @@ def copyJs(jsFile):
     dash = ""
     for i in range(writeCount):
         dash += "-"
-    with open(jsFile, "r", encoding = 'utf8') as js:
-        outputFile.write("//" + dash + "start file: " + jsFile + "\n")
-        for line in js:
-            print(line, end="")
-            if (re.match("import", line)):
-                fileName = findFileName(line)
-                if (importList.count(fileName) < 1):
-                    print("//new import")
-                    importList.append(fileName)
-                    importRoot = fileRootList[fileNameList.index(fileName + ".js")]
-                    copyJs(importRoot)
-                else:
-                    print("//import already exists")
-            elif (re.search("require", line)):
-                if (requireList.count(line) < 1):
-                    print("//new require")
-                    requireList.append(line)
-                    writeIntoFile(line)
-                else:
-                    print("//require already exists")
-            elif (re.search("eslint-enable", line)):
-                print("//skip eslint-enable")
-            else:
-                writeIntoFile(line)
+    outputFile.write("//" + dash + "start file: " + jsFile + "\n")
+
+    readJsFile(jsFile)
+
     outputFile.write("//" + dash + "end file: " + jsFile + "\n")
     space = ""
     for i in range(writeCount):
         space += " "
     outputFile.write("//" + space + "===========================\n")
     writeCount -= 1
+
+
+def readJsFile(jsFile):
+    with open(jsFile, "r", encoding = 'utf8') as js:
+        for line in js:
+            print(line, end="")
+            if (re.match("import", line)):
+                importFile(line)
+            elif (re.search("require", line)):
+                requireFile(line)
+            elif (re.search("eslint-enable", line)):
+                print("//skip eslint-enable")
+            else:
+                writeIntoFile(line)
+
+
+def importFile(line):
+    fileName = findFileName(line)
+    if (importList.count(fileName) < 1):
+        print("//new import")
+        importList.append(fileName)
+        importRoot = fileRootList[fileNameList.index(fileName + ".js")]
+        copyJs(importRoot)
+    else:
+        print("//import already exists")
+
+
+def requireFile(line):
+    if (requireList.count(line) < 1):
+        print("//new require")
+        requireList.append(line)
+        writeIntoFile(line)
+    else:
+        print("//require already exists")
 
 
   
